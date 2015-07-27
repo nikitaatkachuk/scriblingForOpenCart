4c4a674dad9c89a23a6a33b7d45c79bd0f9a43c0
nibilung <mordarulit@mail.ru>
2015-07-27 08:06:09 +0300

    Rename file, change method params

--- src/main/java/by/vigi/utils/FileDownloader.java
+++ src/main/java/by/vigi/utils/FileDownloader.java
@@ -11,24 +11,28 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.UUID;
 
-public final class FileUploader 
+public final class FileDownloader
 {
-	private static final String PATH_ON_SERVER = "/var/www/image/data/scribler/";
-	
-	public static File uploadFile(String category, String shopName, String imageUrl) throws IOException
+	private static final String PATH_ON_SERVER = "/var/www/image/data/demo/pic";
+	public static File uploadFile(String imageUrl) throws IOException
 	{
 		URL url = new URL(imageUrl);
-		File folderForCopy = new File(PATH_ON_SERVER + category + "/" + shopName);
-		File result = new File(folderForCopy, UUID.randomUUID().toString());
+		File folderForCopy = new File(PATH_ON_SERVER);
+		if (!folderForCopy.exists())
+		{
+			folderForCopy.mkdirs();
+		}
+		File result = new File(folderForCopy, UUID.randomUUID().toString().substring(1,10).replaceAll("-","_") + ".jpg");
+		result.createNewFile();
 		InputStream inputStream = url.openStream();
 		OutputStream outputStream = new FileOutputStream(result);
 		
-		byte[] buffer = new byte[2048];
+		byte[] buffer = new byte[4086];
         
         int length = 0;
         
-        while ((length = inputStream.read(buffer)) != -1) {
-           System.out.println("Buffer Read of length: " + length);
+        while ((length = inputStream.read(buffer)) != -1)
+        {
            outputStream.write(buffer, 0, length);
         }
 		return result;
